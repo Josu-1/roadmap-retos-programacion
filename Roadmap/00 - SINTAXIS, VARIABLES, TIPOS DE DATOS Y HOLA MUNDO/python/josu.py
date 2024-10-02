0c94ec3708905e431d5d3742a34221492cd93bc9
Josu <dejesusjosue14@gmail.com>
2024-10-02 17:56:46 -0400

    Create josu.py

---
+++ Roadmap/00 - SINTAXIS, VARIABLES, TIPOS DE DATOS Y HOLA MUNDO/python/josu.py
@@ -0,0 +1,38 @@
+ # https://python.org
+# Comentario en una línea: Este es el sitio web oficial de Python
+
+"""
+Este es un comentario de varias líneas
+"""
+
+'''
+Instrucciones del ejercicio:
+- Crear comentarios utilizando diferentes sintaxis.
+- Crear una variable y una constante.
+- Crear variables con diferentes tipos de datos primitivos.
+- Imprimir un mensaje de saludo.
+'''
+
+# Variables (pueden cambiar de valor)
+my_variable = "Mi variable"
+my_variable = "Nuevo valor de mi variable"
+
+# Constante (se mantiene constante, pero Python no impide que cambie)
+MY_CONSTANT = "Mi constante"  
+
+# Tipos de datos primitivos
+my_int = 1  # Entero
+my_float = 1.5  # Decimal
+my_bool = True  # Booleano
+my_bool = False # Las variables de tipo bool solo puedes ser Cierto o falso 
+my_string = "Mi cadena de texto"  # Cadena de texto (comillas dobles)
+my_other_string = 'Mi otra cadena de texto'  # Cadena de texto (comillas simples)
+
+# Imprimir un mensaje
+print("¡Hola, Python!")
+
+# Imprimir los tipos de las variables
+print(type(my_int))       # <class 'int'>
+print(type(my_float))     # <class 'float'>
+print(type(my_bool))      # <class 'bool'>
+print(type(my_string))    # <class 'str'>
